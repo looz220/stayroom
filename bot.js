75ce2b292fe53ca67564d302225f7cce587a1913
looz220 <46286224+looz220@users.noreply.github.com>
2020-03-25 16:21:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-var x = client.channels.get("676536378321797120");
+var x = client.channels.get("689159360324304906");
 if (x) x.join();
 });
   client.login(process.env.BOT_TOKEN);
